0f48bb30315bff46720e693062f33254b746f54b
Vishnu Rathore <vr8043836@gamil.com>
2022-08-18 12:48:48 +0530

    web3

---
+++ components/layout/themes.js
@@ -0,0 +1,22 @@
+const light = {
+    color: "#000",
+    bgColor: '#efe7fd',
+    bgImage: 'linear-gradient(180deg, #efe7fd 0%, #bdccf7 60%)',
+    bgDiv: '#fff',
+    bgSubDiv: '#efe7fd'
+}
+
+const dark = {
+    color: '#fff',
+    bgColor: '#171F24',
+    bgImage: 'linear-gradient(180deg, #2f0f3d 5%, #000000 90%)',
+    bgDiv: 'grey',
+    bgSubDiv: '#171F24'
+}
+
+const themes = {
+    light: light,
+    dark: dark,
+}
+
+export default themes;
