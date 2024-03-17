89472d9b0f686af36060e0d9bcbdcc3cafcb1a73
Anishkumar Patel <chargaoshou@gmail.com>
2024-03-17 12:48:57 -0400

    GKE update 15

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,7 @@
-variable "vpc_cidr_block" {}
+variable "vpc_cidr_block" {
+  default = "10.10.0.0/16"
+}
+
 variable "private_subnet_cidr_blocks" {
   description = "List of CIDR blocks for the private subnets."
   type        = list(string)
