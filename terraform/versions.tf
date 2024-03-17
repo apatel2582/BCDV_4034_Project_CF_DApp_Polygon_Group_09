c56fbc2acadb884e2da15e71d9d34708b3ddcd2d
Anishkumar Patel <chargaoshou@gmail.com>
2024-03-17 12:12:22 -0400

    GKE update 8

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,11 +1,12 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.74.0"
+      version = "~> 4.74.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.90.0"
     }
   }
 
