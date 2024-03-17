1ef7b22762803d89b0ab995c21122e3a40deff83
Anishkumar Patel <chargaoshou@gmail.com>
2024-03-17 12:52:25 -0400

    GKE update 16

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -5,3 +5,5 @@ project_id = "apt-vine-417512"
 region     = "northamerica-northeast2"
 private_subnet_cidr_blocks = ["10.10.1.0/24", "10.10.2.0/24"]
 public_subnet_cidr_blocks = ["10.10.3.0/24", "10.10.4.0/24"]
+vpc_cidr_block = "10.10.0.0/16"
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,15 @@
 variable "vpc_cidr_block" {
-  default = "10.10.0.0/16"
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.10.0.0/16"
 }
 
 variable "private_subnet_cidr_blocks" {
   description = "List of CIDR blocks for the private subnets."
   type        = list(string)
 }
+
 variable "public_subnet_cidr_blocks" {
   description = "List of CIDR blocks for the public subnets."
   type        = list(string)
 }
-
